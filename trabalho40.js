7628499ea61bf57c700c27185826d14ccbdf0470
GabrielGavinho <121297225+GabrielGavinho@users.noreply.github.com>
2025-05-20 22:25:11 -0300

    Add files via upload

---
+++ trabalho40.js
@@ -0,0 +1,6 @@
+const botao = document.getElementById("botao");
+const frase = document.getElementById("motivacional");
+
+botao.addEventListener("click", () => {
+    frase.style.display = "block";
+});
